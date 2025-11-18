ddc9c3441cb3fab190cae7879ad4dc79a54fbf01
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-11-18 16:30:40 -0500

    Test change

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -2,7 +2,7 @@ import AboutHero from './AboutHeroSection';
 import MissionSection from './MissionSection';
 import StatsSection from './StatsSection';
 import TeamSection from './TeamSection';
-import CTASection from '@/components/CTASection';
+import CTASection from '@/components/CTASection'; 
 
 export default function AboutPage() {
   return (
