401e16ac11f408e664e836e146d1661ce52b2c50
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-11-25 16:24:55 -0500

    Update mobile formatting

--- src/components/CTASection/index.tsx
+++ src/components/CTASection/index.tsx
@@ -7,9 +7,9 @@ export default function CTASection(){
       <div className={styles.sizer}>
         <div className="container">
           <div className="row justify-content-center text-center">
-            <div className="col-8">
-              <h3 id="cta-heading" className="h3 text-white mb-3">Are You Ready to Start Grooming<br />Your Own Dog?</h3>
-              <p className="text-white">Gain the skills and confidence to groom your dog at home while keeping your fur baby<br />safe, happy, and looking their best!</p>
+            <div className="col-12 col-md-10 col-lg-8">
+              <h3 id="cta-heading" className="h3 text-white mb-3">Are You Ready to Start Grooming Your Own Dog?</h3>
+              <p className="text-white">Gain the skills and confidence to groom your dog at home while keeping your fur baby safe, happy, and looking their best!</p>
               <Link href="/courses" className="btn-pill-primary mb-4">
                 Browse Courses
               </Link>
