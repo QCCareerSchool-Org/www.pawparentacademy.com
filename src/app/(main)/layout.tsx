1b55ebc7e8e9b41f983f7ea81df78f677f86f228
Dave Welsh <dave@qccareerschool.com>
2026-03-16 15:55:27 -0400

    Added Brevo Conversations

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import BootstrapClient from '@/components/bootstrapClientX';
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
+import { BrevoConversations } from '@/scripts/brevoConversations';
 
 export const metadata = {
   title: 'Paw Parent Academy',
@@ -13,6 +14,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       <Navbar />
       <main>{children}</main>
       <Footer />
+      {process.env.BREVO_CONVERSATIONS_ID && process.env.BREVO_GROUP_ID && <BrevoConversations conversationsId={process.env.BREVO_CONVERSATIONS_ID} groupId={process.env.BREVO_GROUP_ID} />}
       <BootstrapClient />
     </>
   );
