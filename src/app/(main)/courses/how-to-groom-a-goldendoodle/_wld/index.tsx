6bf80986adaa168e460d90d8ea41545f883c707a
Dave Welsh <dave@qccareerschool.com>
2026-09-02 14:55:39 -0400

    Changed to custom link

--- src/app/(main)/courses/how-to-groom-a-goldendoodle/_wld/index.tsx
+++ src/app/(main)/courses/how-to-groom-a-goldendoodle/_wld/index.tsx
@@ -16,7 +16,7 @@ export const WLDSection: FC = () => (
           <h2>Official Partnership</h2>
           <p>Paw Parent Academy is thrilled to partner with We Love Doodles to provide you with top-tier shampoos, brushes and health essentials specially crafted for Doodles' unique coats. Grooming your Doodle just got a major upgrade with products designed alongside veterinarians and pet enthusiasts so you can keep your furry friend looking and feeling their best!</p>
           <p className="lead"><strong>Use promo code <span style={{ color: 'var(--brand-pink)' }}>QCPS20</span> to save on grooming tools</strong></p>
-          <a href="https://shop.welovedoodles.com" className="btn btn-secondary">Get 20% Off</a>
+          <a href="https://shop.welovedoodles.com/collections/best-sellers?ref=QCPS20" className="btn btn-secondary">Get 20% Off</a>
         </div>
       </div>
     </div>
