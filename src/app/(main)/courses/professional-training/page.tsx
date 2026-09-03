8891e99c36a716e22b740860e935cf9a04c7e0ef
Dave Welsh <dave@qccareerschool.com>
2026-09-03 14:05:29 -0400

    Fixed typo

--- src/app/(main)/courses/professional-training/page.tsx
+++ src/app/(main)/courses/professional-training/page.tsx
@@ -30,7 +30,7 @@ const ProfessionalTrainingPage: PageComponent = () => (
           <div className="col-12 col-lg-6 col-xl-5">
             <h2 className="mb-3">Meet Our Partner</h2>
             <h3>QC Pet Studies</h3>
-            <p>Paw Parent Academy is designed for pet parents who want to learn how to care for their own dogs with confidence.Grooming your own dog, teaching them new skills, and simply spending more time caring for them can be incredibly rewarding. You may even enjoy working with your dog so much that you want t o make it your career.</p>
+            <p>Paw Parent Academy is designed for pet parents who want to learn how to care for their own dogs with confidence. Grooming your own dog, teaching them new skills, and simply spending more time caring for them can be incredibly rewarding. You may even enjoy working with your dog so much that you want t o make it your career.</p>
             <p>If you're interested in working with dogs professionally, our partner school, <strong>QC Pet Studies</strong>, offers comprehensive online training in <strong>dog grooming</strong>, <strong>dog training</strong>, and <strong>dog daycare</strong>. Their flexible programs let you study on your own schedule while learning from experienced industry professionals. You'll build practical, career-ready skills through comprehensive course materials, hands-on assignments, and personalized feedback and mentorship from expert instructors.</p>
           </div>
           <div className="col-12 col-lg-6 col-xl-5">
